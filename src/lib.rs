9d28020c890c3f703e325c8188225dfdbb0aa3cc
Geoffrey Thomas <geofft@ldpreload.com>
2015-02-07 20:32:10 -0500

    Avoid giving string literals a name

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,12 @@ extern fn builtin_lulz(args: *mut bash::word_list) -> libc::c_int {
     0
 }
 
-static NAME: [i8; 5] = ['l' as i8, 'u' as i8, 'l' as i8, 'z' as i8, 0 as i8];
-
 #[no_mangle]
 pub static mut lulz_struct: bash::builtin = bash::builtin {
-    name: &NAME as *const i8,
+    name: &['l' as i8, 'u' as i8, 'l' as i8, 'z' as i8, 0 as i8] as *const i8,
     function: builtin_lulz,
     flags: bash::BUILTIN_ENABLED,
     long_doc: 0 as *const *const i8,
-    short_doc: &NAME as *const i8,
+    short_doc: &['l' as i8, 'o' as i8, 'l' as i8, 'w' as i8, 'u' as i8, 't' as i8, 0 as i8] as *const i8,
     handle: 0 as *const i8,
 };
